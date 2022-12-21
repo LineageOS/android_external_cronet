db713cc90b2013c18db2dacb3bf4e933ec2c1c7a
Mohannad Farrag <aymanm@google.com>
2022-12-21 14:50:17 +0000

    Init Cronet
    go/cronet-for-u-sources
    
    Test: None
    Change-Id: Ie424dc9402afc9e53667b4b8d27e4fd67482f7c3

---
+++ components/metrics/metrics_features.h
@@ -0,0 +1,12 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_METRICS_METRICS_FEATURES_H_
+#define COMPONENTS_METRICS_METRICS_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace metrics::features {}  // namespace metrics::features
+
+#endif  // COMPONENTS_METRICS_METRICS_FEATURES_H_
