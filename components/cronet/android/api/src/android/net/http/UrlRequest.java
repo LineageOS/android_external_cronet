1c157eaeca00714256e976dd4e816d5b56608e93
Motomu Utsumi <motomuman@google.com>
2023-02-20 16:54:17 +0900

    cronet: Address GenericException API linter issue
    Bug: 265674359
    Test: TH
    Change-Id: Idaf1e66f73f75f752818d9495a1e6e3c970a8c60

--- components/cronet/android/api/src/android/net/http/UrlRequest.java
+++ components/cronet/android/api/src/android/net/http/UrlRequest.java
@@ -225,6 +225,8 @@ public abstract class UrlRequest {
          *         will be called with the thrown exception set as the cause of the
          *         {@link CallbackException}.
          */
+        // SuppressLint: Exception will be wrapped and passed to #onFailed, see above javadoc
+        @SuppressLint("GenericException")
         public abstract void onRedirectReceived(@NonNull UrlRequest request,
                 @NonNull UrlResponseInfo info, @NonNull String newLocationUrl) throws Exception;
 
@@ -245,6 +247,8 @@ public abstract class UrlRequest {
          *         will be called with the thrown exception set as the cause of the
          *         {@link CallbackException}.
          */
+        // SuppressLint: Exception will be wrapped and passed to #onFailed, see above javadoc
+        @SuppressLint("GenericException")
         public abstract void onResponseStarted(@NonNull UrlRequest request,
                 @NonNull UrlResponseInfo info) throws Exception;
 
@@ -270,6 +274,8 @@ public abstract class UrlRequest {
          *         {@link #onFailed} will be called with the thrown exception set as the cause of
          *         the {@link CallbackException}.
          */
+        // SuppressLint: Exception will be wrapped and passed to #onFailed, see above javadoc
+        @SuppressLint("GenericException")
         public abstract void onReadCompleted(@NonNull UrlRequest request,
                 @NonNull UrlResponseInfo info, @NonNull ByteBuffer byteBuffer) throws Exception;
 
